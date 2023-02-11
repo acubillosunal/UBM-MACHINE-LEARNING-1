2507809ea4f36d59c56c651e1bb5e67ba505b49a
Alfonso Cubillos Delgado <A2C3D7U5@ARP.fs.net>
2023-02-11 11:39:03 -0500

    First commit

--- init.py
+++ init.py
@@ -1,6 +1,6 @@
-course_id = 'EACD-04-MACHINE-LEARNING-1'
-github_repo = 'UDEA-Esp-Analitica-y-Ciencia-de-Datos/%s'%course_id
-zip_file_url="https://github.com/%s/archive/master.zip"%github_repo
+course_id = 'UBM-MACHINE-LEARNING-1'
+github_repo = 'acubillosunal/%s'%course_id
+zip_file_url="https://github.com/%s/archive/main.zip"%github_repo
 
 def get_last_modif_date(localdir):
     try:
